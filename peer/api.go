d3bf6b7ac0c3906873b663cdd5b210e52966c259
metalwood <377307289@qq.com>
2016-10-16 09:13:22 +0800

    Update api.go

--- peer/api.go
+++ peer/api.go
@@ -16,7 +16,7 @@ func p2pPeerLookupServerInfo(serverId string) (role string, wanIp []string, lanI
 
 func p2pPeerLookupPeers() (connPeerIdList []string, unConnPeerIdList []string, err error) //查询我已经连上和没有连上的Peer的Id, 未连上的其实就是无法内网穿透的客户端
 
-func p2pPeerLookupPeerInfo(peerId string) (wanIp []string, lanIp []string, listenIp string, listenPort int, err error)
+func p2pPeerLookupPeerInfo(peerId string) (wanIp []string, lanIp []string, listenIp string, listenPort int, connType string /* 直连还是中转 */, err error)
 
 func p2pPeerSend(dstIdList []string, data []byte) (failIdList []string, err error) //发送数据给指定uuid的Peer，需要先查询是否直连，如果没有直连再查询哪些server支持此PeerId，然后从支持的server中随机选择。只有最终收到了，才能返回成功。
 
